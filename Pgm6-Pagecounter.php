badc2a6a3fb64eafdaae09a148a9fdc84552da1b
AshwinRameshP <ashwinrameshp@gmail.com>
2019-11-09 13:59:43 +0530

    PGM6

--- Pgm6-Pagecounter.php
+++ Pgm6-Pagecounter.php
@@ -8,5 +8,28 @@
    $fp = fopen("counterlog.txt", "w"); 
    fwrite($fp, $count); 
    fclose($fp); 
+/*
+print "<h3> REFRESH PAGE </h3>";
+
+$name="counterlog.txt";
+
+$file = fopen($name,"r");
+
+$hits= fscanf($file,"%d");
+
+fclose($file);
+
+
+$hits[0]++;
+
+$file = fopen($name,"w");
+
+fprintf($file,"%d",$hits[0]);
+
+fclose($file);
+
+
+print "Total number of views: ".$hits[0];
+*/
 
  ?> 
