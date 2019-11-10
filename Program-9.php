438c2bf637e9d46e4b30ebadf716bf3962018994
AshwinRameshP <ashwinrameshp@gmail.com>
2019-11-10 20:20:07 +0530

    Pgm 8 & 9 updated !!!!

--- Program-9.php
+++ Program-9.php
@@ -1,42 +1,51 @@
+
 <?php
 
-$allTheStates = "Mississippi Alabama Texas Massachusetts Kansas";
-$statesArray = [];
-$states1 = explode(' ', $allTheStates);
-$i = 0;
-//states that ends in xas
-foreach ($states1 as $state) {
- if (preg_match('/xas$/', ($state))) {
- $statesArray[$i] = ($state);
- $i = $i + 1;
- print "\nThe States that ends in xas:" . $state;
- }
+$states = "Mississippi Alabama Texas Massachusetts Kansas"; $statesArray = [];
+
+$states1 = explode(' ',$states);
+
+echo "Original Array :<br>";
+
+foreach ( $states1 as $i => $value )
+
+print("STATES[$i]=$value<br>");
+
+foreach($states1 as $state)  {
+
+if(preg_match( '/xas$/', ($state)))
+
+$statesArray[0] = ($state);
+
 }
-//states that begins with k and ends in s
-foreach ($states1 as $state) {
- if (preg_match('/^k.*s$/i', ($state))) {
- $statesArray[$i] = ($state);
- $i = $i + 1;
- echo "\nThe states that begins with k ans ends in s:" . $state;
- }
-}
-//states that begins with M and ends in s
+
 foreach($states1 as $state) {
-if (preg_match('/^M.*s$/', ($state))) {
- $statesArray[$i] = ($state);
- $i = $i + 1;
- echo "\nThe states that begins with M and ends in s:" . $state;
+
+if(preg_match('/^k.*s$/i', ($state)))
+
+$statesArray[1] = ($state);
+
 }
-}
-//states that ends in a
 foreach($states1 as $state) {
-  if (preg_match('/a$/', ($state))) {
- $statesArray[$i] = ($state);
- $i = $i + 1;
- echo "\nThe states that ends in a:" . $state;
+
+if(preg_match('/^M.*s$/', ($state)))
+
+$statesArray[2] = ($state);
+
 }
+
+foreach($states1 as $state){
+
+if(preg_match('/a$/', ($state)))
+
+$statesArray[3] = ($state);
+
 }
-foreach ($statesArray as $element => $value) {
- print( "\n" . $value . " is the element " . $element);
-}
+
+echo "<br><br>Resultant Array :<br>";
+
+foreach ( $statesArray as $array => $value )
+
+print("STATES[$array]=$value<br>");
+
 ?>
